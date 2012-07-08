0cadd6ede37028e5ab475e9b99f4b225b7dcfd21
mtbishop <>
2012-07-08 05:31:24 +0000

    initial merge of 2951972

--- vtun.h
+++ vtun.h
@@ -219,6 +219,7 @@ struct vtun_opts {
    struct vtun_addr bind_addr;	 /* Server should listen on this address */
    int  svr_type;	 /* Server mode */
    int  syslog; 	 /* Facility to log messages to syslog under */
+   int  quiet;		 /* Be quiet about common errors */
 };
 #define VTUN_STAND_ALONE	0 
 #define VTUN_INETD		1	
